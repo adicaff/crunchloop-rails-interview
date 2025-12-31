d1de4d7231993ddfa06c894a9de2920825180ce5
Adrian Caffarelli <adicaff@hotmail.com>
2025-12-31 10:25:29 -0300

    redirect to login with unknown routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:passwords]
   namespace :api do
     resources :todo_lists, path: :todolists do
       resources :list_items, path: :items, only: %i[index show create update destroy]
@@ -14,4 +14,13 @@ Rails.application.routes.draw do
   end
 
   root 'todo_lists#index'
+  
+  # Catch-all route: redirect unmatched routes to login
+  # This must be last to only catch routes that don't match anything above
+  match '*path', to: redirect('/users/sign_in'), via: :all, constraints: lambda { |req|
+    # Exclude Rails internal paths and static assets
+    !req.path.start_with?('/rails') && 
+    !req.path.start_with?('/assets') &&
+    !req.path.start_with?('/api')
+  }
 end
